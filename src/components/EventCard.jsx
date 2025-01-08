17335252310b4e1c6e626d596100efbce1f1f7b2
Parbat Tamang <parbattamang1234@gmail.com>
2025-01-08 11:44:28 +0545

    test commit

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,7 +1,7 @@
 function EventCard() {
     return (
         <div className="flex flex-col bg-gray-400 h-[524px] w-[340px] rounded-t-xl">
-            <img src="https://cdn.ticketsanjal.com/images/2024/12/29/094229-Jamesey.jpg" alt="not found" className="rounded-t-xl object-cover h-[340px]" />
+            <img src="https://cdn.ticketsanjal.com/images/2024/12/29/094229-Jamesey.jpg" alt="Not found" className="rounded-t-xl object-cover h-[340px]" />
             <div className="bg-[#FFFFFF] pt-[0.75rem] pb-[0.75rem] pl-[1rem] pr-[1rem]">
                 <div className="flex flex-row justify-between">
                     <span>10 JAN</span>
